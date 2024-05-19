c790a2e29076ea9c2b768900798e90d070e4bcaa
Mohamed Eldundy <154285995+Mohamed22Tarek@users.noreply.github.com>
2024-05-19 22:43:51 +0300

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,12 @@
+let imgBig=document.getElementById('imgBig');
+let container=document.querySelector('.container');
+
+
+
+function phones(phone){
+
+    imgBig.src=phone;
+}
+function colors(color){
+   container.style.background=color 
+}
